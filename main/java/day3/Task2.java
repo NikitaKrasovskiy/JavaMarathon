2be91c554f80d772216eae2de587b4c502ce085e
NikitaKrasovskiy <nikitakrasovskij5@gmail.com>
2023-03-15 15:36:55 +0300

    day3  yes

--- main/java/day3/Task2.java
+++ main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            double number = scanner.nextDouble();
+            double number2 = scanner.nextDouble();
+            if (number2 == 0) {
+                break;
+            }
+            System.out.println(number / number2);
+        }
     }
 }

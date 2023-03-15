c8f50f819d5fffac0f729fba9b28881f0bfeebdf
NikitaKrasovskiy <nikitakrasovskij5@gmail.com>
2023-03-15 13:22:57 +0300

    first commit day1(compl)

---
+++ main/java/day0/Task1.java
@@ -0,0 +1,19 @@
+package day0;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.print("JAVA" + " ");
+            i++;
+        }
+        System.out.println();
+        for (int j = 1; j < 10; j++) {
+            System.out.print("JAVA" + " ");
+        }
+
+        for (int j = 0; j <= 10; j++) {
+            System.out.println("JAVA");
+        }
+    }
+}

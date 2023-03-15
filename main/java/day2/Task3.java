07929650de2d49ecc04b03c23631e7da27deb21c
NikitaKrasovskiy <nikitakrasovskij5@gmail.com>
2023-03-15 15:19:42 +0300

    day2  good

--- main/java/day2/Task3.java
+++ main/java/day2/Task3.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        if (number1 >= number2) {
+            System.out.println("Некорректный ввод");
+        }
+        number1 += 1;
+        while (number1 < number2) {
+            if (number1 % 5 == 0 && number1 % 10 != 0) {
+                System.out.print(number1 + " ");
+            }
+            number1++;
+        }
     }
 }
